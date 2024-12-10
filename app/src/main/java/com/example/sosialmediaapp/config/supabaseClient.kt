bb7b6d7ccfa60f6213fa657ce74c9ae9e78b3fd8
anantariskys <gamersindo555@gmail.com>
2024-12-10 08:26:45 +0700

    Update supabaseClient.kt

--- app/src/main/java/com/example/sosialmediaapp/config/supabaseClient.kt
+++ app/src/main/java/com/example/sosialmediaapp/config/supabaseClient.kt
@@ -7,8 +7,8 @@ import io.github.jan.supabase.storage.Storage
 
 
 val supabase = createSupabaseClient(
-    supabaseUrl = "https://yyvuyecllzzyjvpvrbga.supabase.co",
-    supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5dnV5ZWNsbHp6eWp2cHZyYmdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1MTExMjcsImV4cCI6MjA0ODA4NzEyN30.H7lnC8z0iK6-o9xY0lRuVQxIqt2DW0VHa7VzWnYiGDU"
+    supabaseUrl = "https://knkgrcdvcevpkocsqfzf.supabase.co",
+    supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtua2dyY2R2Y2V2cGtvY3NxZnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM2NjcxOTAsImV4cCI6MjA0OTI0MzE5MH0.mrwC0UhdYsDMy2dufc8lGPKfBwGR3CcH5iGkDXqrasM"
 ) {
 
     install(Auth) // Untuk autentikasi
