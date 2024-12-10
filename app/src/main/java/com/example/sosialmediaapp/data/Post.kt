3be4ea01e3830e4e2f61cce3a74f70444a70d1dd
anantariskys <gamersindo555@gmail.com>
2024-12-10 08:26:51 +0700

    Update Post.kt

--- app/src/main/java/com/example/sosialmediaapp/data/Post.kt
+++ app/src/main/java/com/example/sosialmediaapp/data/Post.kt
@@ -1,13 +1,12 @@
 package com.example.sosialmediaapp.data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Post(
-    var id: String = "",           // Provide a default value for the id
-    val content: String = "",      // Provide a default value for the content
-    val imageUrl: String? = null,  // Provide a default value (null) for the imageUrl
-    val userId: String = "",       // Provide a default value for the userId
-    val timestamp: Long = 0,       // Provide a default value for the timestamp
-    val likes: Int = 0,            // Provide a default value for the likes
-    val likedBy: List<String> = listOf() ,
-    val comments: Int = 0
+    val id: Int,
+    val user_id:Int,
+    val caption: String,
+    val image: String,
 )
 
