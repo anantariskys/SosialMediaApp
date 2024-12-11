b9b78ca0ac5e3636e53c2ad23dde5a0be72a79e2
anantariskys <gamersindo555@gmail.com>
2024-12-11 12:57:21 +0700

    Update User.kt

--- app/src/main/java/com/example/sosialmediaapp/data/User.kt
+++ app/src/main/java/com/example/sosialmediaapp/data/User.kt
@@ -1,8 +1,21 @@
 package com.example.sosialmediaapp.data
 
-data class User(
-    val uid: String = "",
-    val name: String = "",
-    val photoUrl: String? = null,
+import kotlinx.serialization.Serializable
 
+
+
+@Serializable
+data class User(
+    val id: String,
+    val email: String,
+    val profiles: Profiles
+)
+
+@Serializable
+data class  Profiles(
+    val id: Int,
+    val user_id: String,
+    val display_name: String,
+    val full_name : String,
+    val image :String?
 )
